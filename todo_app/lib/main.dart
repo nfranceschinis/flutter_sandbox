75ac4f96bd2b014c02217942b715dbb7ebc7969a
Nicola Franceschinis <nfranceschinis@exelio.eu>
2026-02-27 17:36:36 +0100

    New version

--- todo_app/lib/main.dart
+++ todo_app/lib/main.dart
@@ -1,186 +1,102 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+class Entry {
+  const Entry({required this.name});
+
+  final String name;
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+typedef ListChangedCallback = void Function(Entry entry, bool isDone);
 
-  // This widget is the root of your application.
+class ToDoListItem extends StatelessWidget {
+  ToDoListItem({
+    required this.entry,
+    required this.isDone,
+    required this.onToDoListChanged,
+  }) : super(key: ObjectKey(entry));
+
+  final Entry entry;
+  final bool isDone;
+  final ListChangedCallback onToDoListChanged;
+
+  Color _getColor(BuildContext context) {
+    return isDone ? Colors.black54 : Theme.of(context).primaryColor;
+  }
+
+  TextStyle? _getTextStyle(BuildContext context) {
+    if (!isDone) return null;
+
+    return const TextStyle(
+      color: Colors.black54,
+      decoration: TextDecoration.lineThrough,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Cumbinitu',
-      theme: ThemeData(primarySwatch: Colors.deepOrange),
-      home: const MyHomePage(title: 'Cumbinitu?'),
-      debugShowCheckedModeBanner: false,
+    return ListTile(
+      onTap:() {
+        onToDoListChanged(entry, isDone);
+      },
+      leading: CircleAvatar(
+        backgroundColor: _getColor(context),
+        child: Text(entry.name[0]),
+      ),
+      title: Text(entry.name, style: _getTextStyle(context)),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class ToDoList extends StatefulWidget {
+  const ToDoList({required this.entries, super.key});
 
-  final String title;
+  final List<Entry> entries;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<ToDoList> createState() => _ToDoListState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  List<String> todoList = [];
+class _ToDoListState extends State<ToDoList> {
+  final _todoList = <Entry> {};
 
-  final TextEditingController _controller = TextEditingController();
-
-  int updateIndex = -1;
-
-  void addList(String task) {
+  void _handleToDoListChanged(Entry entry, bool isDone) {
     setState(() {
-      todoList.add(task);
-      _controller.clear();
-    });
-  }
-
-  void updateListItem(String task, int index) {
-    setState(() {
-      todoList[index] = task;
-
-      updateIndex = -1;
-      _controller.clear();
-    });
-  }
-
-  void deleteItem(index) {
-    setState(() {
-      todoList.removeAt(index);
+      if (!isDone) {
+        _todoList.add(entry);
+      } else {
+        _todoList.remove(entry);
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(
-          widget.title,
-          style: TextStyle(
-          fontWeight: FontWeight.bold,
-          fontSize: 30,
-        ),
-          ),
-          centerTitle: false,
-      ),
-      body: Container(
-        margin: EdgeInsets.all(10),
-        child: Column(
-          children: [
-            Expanded(
-              flex: 90,
-              child: ListView.builder(
-                itemCount: todoList.length,
-                itemBuilder: (context, index) {
-                  return Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-
-                    color: Colors.green,
-                    child: Container(
-                      margin: EdgeInsets.only(left: 20),
-                      alignment: Alignment.center,
-                      padding: EdgeInsets.all(10),
-                      child: Row(
-                        children: [
-                          Expanded(
-                            flex:80,
-                            child: Text(
-                              //Display task text
-                              todoList[index],
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 20),
-                              ),
-                          ),
-                          //Edit button
-                          IconButton(
-                            onPressed: () {
-                              setState(() {
-                                _controller.clear();
-                                _controller.text = todoList[index];
-                                updateIndex = index;
-                              });
-                            },
-                            icon: Icon(
-                              Icons.edit,
-                              size: 30,
-                              color: Colors.white,
-                            ),
-                          ),
-                          SizedBox(width: 10),
-                          //Delete button
-                          IconButton(
-                            onPressed: () {
-                              deleteItem(index);
-                            },
-                            icon: Icon(
-                              Icons.delete,
-                              size: 30,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ],
-                      )
-                    )
-                  );
-                },
-              ),
-            ),
-            Expanded(
-              flex: 10,
-              child: Row(
-                children: [
-                  Expanded(
-                    flex: 70,
-                    child: SizedBox(
-                      height: 60,
-                      child: TextFormField(
-                        controller: _controller,
-                        decoration: InputDecoration(
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(8),
-                            borderSide: const BorderSide(
-                              color: Colors.green,
-                              )),
-                            filled: true,
-
-                            labelText: 'Crea nuovo task...',
-                            labelStyle: TextStyle(
-                              color: Colors.black,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                      ),
-                    ),
-                  ),
-                FloatingActionButton(
-              backgroundColor: Colors.green,
-              foregroundColor: Colors.white,
-              
-              onPressed: () {
-                assert(_controller.text != '', );
-                updateIndex != -1 ? updateListItem(_controller.text, updateIndex) : addList(_controller.text);
-              },
-              child: Icon(updateIndex != -1 ? Icons.edit : Icons.add),
-            ),
-                ],
-              ),
-            ),
-            SizedBox(width: 5),
-          ],
-        ),
+      appBar: AppBar(title: const Text('Ce ti coventie?')),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        children: widget.entries.map((entry) {
+          return ToDoListItem(
+            entry: entry,
+            isDone: _todoList.contains(entry),
+            onToDoListChanged: _handleToDoListChanged,
+          );
+        }).toList(),
       ),
     );
   }
+}
+void main() {
+  runApp(
+    const MaterialApp(
+      title: 'Ce ti coventie?',
+      home: Center(child:ToDoList(
+        entries: [
+          Entry(name: 'test1'),
+          Entry(name: 'test2'),
+          Entry(name: 'test3'),
+        ],
+      ),),
+    ),
+  );
 }
